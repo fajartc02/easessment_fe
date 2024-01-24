fba1f591a443499114a93ec246caa099c6f263c0
Ade Mahendra <hendraadem@gmail.com>
2024-01-25 00:32:38 +0700

    Add: add apexchart

--- src/views/dashboard/MainDashboard.vue
+++ src/views/dashboard/MainDashboard.vue
@@ -28,10 +28,56 @@
           </div>
         </div>
       </div>
-      <div class="card-body">
+      <div class="card-body px-4">
         <div class="row">
-          <div class="col-2">1</div>
-          <div class="col-6">2</div>
+          <div class="col-md-2 mr-2">
+            <apexchart
+              type="bar"
+              :options="options"
+              :series="series"
+              height="100%"
+            ></apexchart>
+          </div>
+          <div
+            style="height: 300px"
+            class="col-md-10 pt-2 pb-4 bg-warning d-flex horizontal-scrollable"
+          >
+            <div class="col-2 m-2">
+              <div class="w-100 h-100 bg-success rounded">
+                <apexchart
+                  type="bar"
+                  :options="options"
+                  :series="series"
+                  height="100%"
+                ></apexchart>
+              </div>
+              <div>label</div>
+            </div>
+            <div class="col-2 m-2">
+              <div class="w-100 h-100 bg-success rounded"></div>
+              <div>label</div>
+            </div>
+            <div class="col-2 m-2">
+              <div class="w-100 h-100 bg-success rounded"></div>
+              <div>label</div>
+            </div>
+            <div class="col-2 m-2">
+              <div class="w-100 h-100 bg-success rounded"></div>
+              <div>label</div>
+            </div>
+            <div class="col-2 m-2">
+              <div class="w-100 h-100 bg-success rounded"></div>
+              <div>label</div>
+            </div>
+            <div class="col-2 m-2">
+              <div class="w-100 h-100 bg-success rounded"></div>
+              <div>label</div>
+            </div>
+            <div class="col-2 m-2">
+              <div class="w-100 h-100 bg-success rounded"></div>
+              <div>label</div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -50,6 +96,31 @@ export default {
     return {
       selectedMonth: null,
       selectedLine: '-1',
+      options: {
+        chart: {
+          id: 'vuechart-example',
+          type: 'bar',
+          stacked: true,
+          width: '100%',
+          height: '100%',
+          //   stackType: '100%',
+        },
+        plotOptions: {
+          bar: {
+            vertical: true,
+          },
+        },
+      },
+      series: [
+        {
+          data: [
+            {
+              x: 'Overal Precentage',
+              y: 6653,
+            },
+          ],
+        },
+      ],
     }
   },
   computed: {
