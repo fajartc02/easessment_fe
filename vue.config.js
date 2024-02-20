cae4b6f19c4ba4c8029ac8ce3714abd2e4ee4d92
fajartc02 <fajartc02@gmail.com>
2024-02-20 22:12:02 +0700

    old-fe

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer: {
+    proxy: 'http://103.190.28.222:8080'
+  }
 })
