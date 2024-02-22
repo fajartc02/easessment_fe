b65d887887dc278fd701bcc23434d42d0dc18c9a
Ade Mahendra <hendraadem@gmail.com>
2024-02-22 22:53:17 +0700

    Fix: bug fix

--- src/views/observations/DetailObservation.vue
+++ src/views/observations/DetailObservation.vue
@@ -161,31 +161,85 @@
           <td class="text-center">{{ i + 1 }}</td>
           <td class="p-2">{{ item.category_nm }}</td>
           <td>
-            <CFormSelect :disabled="isCheck" v-model="item.judgment_id">
+            <div
+              v-if="
+                item.job_type_nm !== 'Type 3' &&
+                item.category_nm == 'Standarize Work'
+              "
+            >
+              <div class="d-flex">
+                <input
+                  type="number"
+                  v-model="item.stw_ct1"
+                  class="form-control text-center"
+                  style="width: 70px"
+                  placeholder="CT1"
+                />
+                <input
+                  type="number"
+                  v-model="item.stw_ct2"
+                  class="form-control text-center mx-2"
+                  style="width: 70px"
+                  placeholder="CT2"
+                />
+                <input
+                  type="number"
+                  v-model="item.stw_ct3"
+                  class="form-control text-center"
+                  style="width: 70px"
+                  placeholder="CT3"
+                />
+                <input
+                  type="number"
+                  v-model="item.stw_ct4"
+                  class="form-control text-center mx-2"
+                  style="width: 70px"
+                  placeholder="CT4"
+                />
+                <input
+                  type="number"
+                  v-model="item.stw_ct5"
+                  class="form-control text-center"
+                  style="width: 70px; margin-right: 10px"
+                  placeholder="CT5"
+                  @keyup="getCategories()"
+                />
+                <div class="mx-1 d-flex flex-column">
+                  <span class="badge bg-secondary"
+                    >Avg: {{ judgementAverage }}</span
+                  >
+                  <span class="badge bg-secondary mt-1"
+                    >Prc: {{ judgementPrecentage }} %</span
+                  >
+                </div>
+              </div>
+            </div>
+            <CFormSelect v-else :disabled="isCheck" v-model="item.judgment_id">
               <option>Select Judgment</option>
               <option v-for="judg in judgments" :key="judg.id" :value="judg.id">
                 {{ judg.text }}
               </option>
             </CFormSelect>
           </td>
-          <td
-            v-if="
-              item.judgment_id == '2e247c66-3e9c-44b6-951a-0a26791ad37d' &&
-              item.judgment_id
-            "
-          >
-            <CFormSelect :disabled="isCheck" v-model="item.factor_id">
-              <option>Select Factor</option>
-              <option
-                v-for="factor in factors"
-                :key="factor.text"
-                :value="factor.id"
-              >
-                {{ factor.text }}
-              </option>
-            </CFormSelect>
+          <td>
+            <div
+              v-if="
+                item.judgment_id == '2e247c66-3e9c-44b6-951a-0a26791ad37d' &&
+                item.judgment_id
+              "
+            >
+              <CFormSelect :disabled="isCheck" v-model="item.factor_id">
+                <option>Select Factor</option>
+                <option
+                  v-for="factor in factors"
+                  :key="factor.text"
+                  :value="factor.id"
+                >
+                  {{ factor.text }}
+                </option>
+              </CFormSelect>
+            </div>
           </td>
-          <td v-else></td>
           <td
             v-if="
               item.judgment_id == '2e247c66-3e9c-44b6-951a-0a26791ad37d' &&
@@ -207,12 +261,7 @@
                     () => {
                       addFindingsModal = true
                       mapUsersData()
-                      editFinding(
-                        findings.filter((find) => {
-                          return find.category_id == item.id
-                        }),
-                        item.id,
-                      )
+                      editFinding(item.id)
                     }
                   "
                 >
@@ -399,18 +448,28 @@
                 </div>
               </CModalBody>
               <CModalFooter>
-                <CButton
-                  color="primary"
-                  class="text-white"
-                  @click="updateFindingData(item.id, item.factor_id, finding)"
-                  >Update finding data</CButton
+                <div
+                  v-if="
+                    findings.filter((fin) => {
+                      return fin.category_id == item.id
+                    }).length == 0
+                  "
                 >
-                <CButton
-                  color="primary"
-                  class="text-white"
-                  @click="addFindingData(item.id, item.factor_id, finding)"
-                  >Save finding data</CButton
-                >
+                  <CButton
+                    color="primary"
+                    class="text-white"
+                    @click="addFindingData(item.id, item.factor_id, finding)"
+                    >Save finding data</CButton
+                  >
+                </div>
+                <div v-else>
+                  <CButton
+                    color="primary"
+                    class="text-white"
+                    @click="updateFindingData(item.id, item.factor_id, finding)"
+                    >Update finding data</CButton
+                  >
+                </div>
                 <CButton
                   color="secondary"
                   class="text-white mx-2"
@@ -481,6 +540,9 @@ export default {
       tskkLabel: 'TSKK',
       addFindingsModal: false,
       selectedPIC: null,
+      judgementAverage: 0,
+      judgementPrecentage: 0,
+      judgementID: null,
       judgementVal: {
         stw_ct1: 50,
         stw_ct2: 50,
@@ -555,7 +617,6 @@ export default {
         `/observation/report/${this.observation.observation_id}`,
       )
     },
-
     async getDetail() {
       await this.$store.dispatch(GET_OBSERVATION_DETAIL, this.$route.params.id)
     },
@@ -575,21 +636,22 @@ export default {
       this.groups = groups.data.data
     },
     resetData() {
-      this.finding_date = moment().format('YYYY-MM-DD')
-      // this.finding_location = ''
-      this.finding_desc = ''
-      this.cm_desc = ''
-      this.cm_priority = 0
-      this.cm_str_plan_date = ''
-      this.cm_end_plan_date = ''
-      this.cm_str_act_date = null
-      this.cm_end_act_date = null
-      this.cm_training_date = null
-      this.cm_judg = false
-      this.cm_sign_lh_red = null
-      this.cm_sign_lh_white = null
-      this.cm_sign_sh = null
-      this.cm_comments = null
+      this.finding.finding_date = moment().format('YYYY-MM-DD')
+      this.finding.finding_location = ''
+      this.finding.finding_desc = ''
+      this.finding.cm_desc = ''
+      this.finding.cm_priority = null
+      this.finding.cm_str_plan_date = ''
+      this.finding.cm_end_plan_date = ''
+      this.finding.cm_str_act_date = null
+      this.finding.cm_end_act_date = null
+      this.finding.cm_training_date = null
+      this.finding.cm_judg = false
+      this.finding.cm_sign_lh_red = null
+      this.finding.cm_sign_lh_white = null
+      this.finding.cm_sign_sh = null
+      this.finding.cm_comments = null
+      this.selectedPIC = null
     },
     addFindingData(categoryID, factorID, findings) {
       let data = {
@@ -605,22 +667,14 @@ export default {
       this.addFindingsModal = false
       this.resetData()
     },
-    editFinding(categoryID, data) {
-      // const index = this.findings.findIndex((object) => {
-      //   return object.category_id === `${categoryID}`
-      // })
+    editFinding(categoryID) {
+      const data = this.findings.find((obj) => {
+        return obj.category_id == categoryID
+      })
 
-      const index = this.findings.findIndex(
-        (Obj) => Obj.category_id === categoryID,
-      )
+      console.log(data)
 
-      console.log('index' + index)
-      console.log('data' + data)
-
-      console.log(this.findings)
-      console.log(categoryID)
-
-      // this.findings = data
+      this.finding = data
     },
     updateFindingData(categoryID, factorID, findings) {
       let data = {
@@ -632,30 +686,84 @@ export default {
         ...findings,
       }
 
-      console.log(data)
-
       const index = this.findings.findIndex((object) => {
         return object.category_id === categoryID
       })
+
+      console.log(data)
 
       console.log(index)
     },
     async getCategories() {
       ApiService.setHeader()
+      const OK_ID = 'c4f5ff30-1b95-4ad8-8af8-e3e9d90bd942'
+      const NG_ID = '2e247c66-3e9c-44b6-951a-0a26791ad37d'
       const categories = await ApiService.get(`master/categories`)
       const mapCategory = categories.data.data.map((itm, i) => {
         itm.judgment_id = null
         itm.factor_id = null
+        itm.stw_ct1 = 0
+        itm.stw_ct2 = 0
+        itm.stw_ct3 = 0
+        itm.stw_ct4 = 0
+        itm.stw_ct5 = 0
+
+        let result = this.resultCheck[i]
 
         if (this.resultCheck?.length > 0) {
-          let result = this.resultCheck[i]
+          let total =
+            result.stw_ct1 +
+            result.stw_ct2 +
+            result.stw_ct3 +
+            result.stw_ct4 +
+            result.stw_ct5
+          let totalAvg = total / 5
+
+          let totalPrecentage =
+            ((Math.max(
+              result.stw_ct1,
+              result.stw_ct2,
+              result.stw_ct3,
+              result.stw_ct4,
+              result.stw_ct5,
+            ) -
+              Math.min(
+                result.stw_ct1,
+                result.stw_ct2,
+                result.stw_ct3,
+                result.stw_ct4,
+                result.stw_ct5,
+              )) /
+              2 /
+              totalAvg) *
+            100
+
+          this.judgementAverage = totalAvg
+          this.judgementPrecentage = totalPrecentage.toFixed()
+          if (
+            this.selectedCategoryID == '3ce06d66-fe82-4e0a-a1fd-ce927ec85167'
+          ) {
+            if (totalPrecentage.toFixed() > 20) {
+              this.judgementID = NG_ID
+            }
+          } else {
+            this.judgementID = OK_ID
+          }
 
           itm.judgment_id = result.judgment_id
           itm.factor_id = result.factor_id
+          itm.stw_ct1 = result.stw_ct1
+          itm.stw_ct2 = result.stw_ct2
+          itm.stw_ct3 = result.stw_ct3
+          itm.stw_ct4 = result.stw_ct4
+          itm.stw_ct5 = result.stw_ct5
         }
+
         return itm
       })
+
       this.categories = mapCategory
+      console.log(mapCategory)
       await this.getJudgments()
       await this.getFactors()
       await this.getGroups()
@@ -670,11 +778,11 @@ export default {
           let newObj = {
             category_id: element.category_id,
             judgment_id: element.judgment_id,
-            stw_ct1: 0,
-            stw_ct2: 0,
-            stw_ct3: 0,
-            stw_ct4: 0,
-            stw_ct5: 0,
+            stw_ct1: element.stw_ct1,
+            stw_ct2: element.stw_ct2,
+            stw_ct3: element.stw_ct3,
+            stw_ct4: element.stw_ct4,
+            stw_ct5: element.stw_ct5,
           }
           this.resultCheck.push(newObj)
         }
