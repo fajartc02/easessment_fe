0ee260d3e012167bdf54da5ab51d12af37ea9245
fajartc02 <fajartc02@gmail.com>
2024-04-11 13:59:11 +0700

    Reusable modal detail, head finding table

---
+++ src/components/ModalFinding/ModalFindingDetail.vue
@@ -0,0 +1,139 @@
+<template>
+  <CModal backdrop="static" alignment="center" :visible="detailTemuanModal" @close="$emit('close-modal')" size="lg"
+    scrollable>
+    <CModalHeader>
+      <CModalTitle>Detail temuan</CModalTitle>
+    </CModalHeader>
+    <CModalBody>
+      <div class="row">
+        <div class="col-lg-6 col-md-6 col-sm-12">
+          <div class="mb-2">
+            <label class="mb-1">Line</label>
+            <input type="text" class="form-control" :value="findingDetail?.line_nm" disabled />
+          </div>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-12">
+          <div class="mb-2">
+            <label class="mb-1">Source cat</label>
+            <input type="text" class="form-control" :value="findingDetail?.source_category" disabled />
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-lg-6 col-md-6 col-sm-12">
+          <div class="mb-2">
+            <label class="mb-1">Tanggal temuan</label>
+            <input type="date" class="form-control" :value="formatTheDate(findingDetail?.finding_date)" disabled />
+          </div>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-12">
+          <div class="mb-2">
+            <label class="mb-1">Pos</label>
+            <input type="text" class="form-control" :value="findingDetail?.finding_location" disabled />
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-lg-6 col-md-6 col-sm-12">
+          <div class="mb-2">
+            <label class="mb-1">Priority</label>
+            <select class="form-select" :value="findingDetail?.cm_priority" :disabled="true">
+              <option selected>Select priority</option>
+              <option value="P1">P1: Safety and Quality Issue</option>
+              <option value="P2">P2: Productivity Issue</option>
+              <option value="P3">P3: Cost Issue</option>
+            </select>
+          </div>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-12">
+          <div class="mb-2">
+            <label class="mb-1">PIC </label>
+            <input type="text" class="form-control" :value="findingDetail?.cm_pic_nm" disabled />
+          </div>
+        </div>
+      </div>
+
+
+      <div class="mb-2">
+        <label class="mb-1">Finding description / problem</label>
+        <textarea cols="30" rows="5" class="form-control" :value="findingDetail?.finding_desc" disabled></textarea>
+      </div>
+      <div class="mb-2">
+        <label class="mb-1">Rencana perbaikan</label>
+        <input type="date" class="form-control" :value="formatTheDate(findingDetail?.cm_str_plan_date)" disabled />
+      </div>
+
+
+      <div class="mb-2">
+        <label class="mb-1">CM description</label>
+        <textarea cols="30" rows="5" class="form-control" :value="findingDetail?.cm_desc" disabled></textarea>
+      </div>
+
+      <div class="mb-2">
+        <label class="mb-1">CM Start Plan Date </label>
+        <input type="date" class="form-control" :value="formatTheDate(findingDetail?.cm_str_plan_date)" disabled />
+      </div>
+      <div class="mb-2">
+        <label class="mb-1">CM End Plan Date </label>
+        <input type="date" class="form-control" :value="formatTheDate(findingDetail?.cm_end_plan_date)" disabled />
+      </div>
+
+      <hr />
+
+      <div class="mb-2">
+        <label class="mb-1">CM Start actual date</label>
+        <input type="date" class="form-control" :value="formatTheDate(findingDetail?.cm_str_act_date)" disabled />
+      </div>
+      <div class="mb-2">
+        <label class="mb-1">CM End actual date</label>
+        <input type="date" class="form-control" :value="formatTheDate(findingDetail?.cm_end_act_date)" disabled />
+      </div>
+      <div class="mb-2">
+        <label class="mb-1">CM Training date</label>
+        <input type="date" class="form-control" disabled />
+      </div>
+      <div class="mb-2">
+        <label class="mb-1">CM Judge</label>
+        <select class="form-select" :value="findingDetail?.cm_judg" disabled>
+          <option value="true">Sudah</option>
+          <option value="false">Belum</option>
+        </select>
+      </div>
+      <div class="mb-2">
+        <label class="mb-1">CM Sign LH Red</label>
+        <input type="file" class="form-control" disabled />
+      </div>
+      <div class="mb-2">
+        <label class="mb-1">CM Sign LH White</label>
+        <input type="file" class="form-control" disabled />
+      </div>
+      <div class="mb-2">
+        <label class="mb-1">CM Sign SH</label>
+        <input type="file" class="form-control" disabled />
+      </div>
+      <div class="mb-2">
+        <label class="mb-1">CM Comments</label>
+        <input type="text" class="form-control" disabled />
+      </div>
+    </CModalBody>
+    <CModalFooter>
+      <router-link v-if="findingDetail?.observation_id !== null" :to="`/observation/${findingDetail?.observation_id}`"
+        @click="$emit('close-modal')">
+        <CButton color="primary" class="text-white"> View Activity </CButton>
+      </router-link>
+    </CModalFooter>
+  </CModal>
+</template>
+<script>
+export default {
+  name: "ModalFindingDetail",
+  props: {
+    detailTemuanModal: Boolean,
+    findingDetail: Object,
+    formatTheDate: Function
+  }
+}
+</script>
+<style></style>
