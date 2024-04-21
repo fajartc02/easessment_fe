b2746935d750caa63f0f6e1ef44fafb769bc9512
fajartc02 <fajartc02@gmail.com>
2024-04-21 23:29:05 +0700

    remove footer

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -6,13 +6,11 @@
       <div class="body flex-grow-1 px-3">
         <router-view />
       </div>
-      <AppFooter />
     </div>
   </div>
 </template>
 
 <script>
-import AppFooter from '@/components/AppFooter.vue'
 import AppHeader from '@/components/AppHeader.vue'
 import AppSidebar from '@/components/AppSidebar.vue'
 
@@ -29,7 +27,6 @@ export default {
     if (!this.isAuthenticated) this.$router.push('/login')
   },
   components: {
-    AppFooter,
     AppHeader,
     AppSidebar,
   },
