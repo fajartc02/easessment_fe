694175154ea9fff534f08c24ac9395eb558364e0
fajartc02 <fajartc02@gmail.com>
2025-02-04 18:37:22 +0700

    update tooltip

--- src/views/observations/stw/StwDashboard.vue
+++ src/views/observations/stw/StwDashboard.vue
@@ -170,9 +170,7 @@
                   <td id="fixCol-5">{{ observationRedShift.group_nm }}</td>
                   <td v-for="item in containerDate" :key="item.idx" style="min-width: 63px">
                     <template v-for="child in observationRedShift.children" :key="child.observation_id">
-                      <ejs-tooltip ref="tooltip" content='Tooltip content 1'>
-                        Tooltip
-                      </ejs-tooltip>
+
                       <template v-if="child.idxdate === String(item.idx)">
                         <TooltipStwSchedule :child="child" :customTooltipStyle="customTooltipStyle" :observation="child"
                           :currentDate="currentDate" @detail-schedule="detailSchedule" />
